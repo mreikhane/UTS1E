df62231f04a6dfcb9f3762da0de7554777d6f5bc
MUHAMAD REIKHAN EFENDI <muhamadreikhan69@gmail.com>
2022-11-14 15:04:21 +0700

    tugas UTS

--- src/jawabannomor3.java
+++ src/jawabannomor3.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class jawabannomor3 {
     public static void main(String[] args) {
-        System.out.println("hallo kelas 1E");
+        int i;
+        System.out.println("(KAMIR FROM CINDE)");
+        System.out.println("--------KFC-------");
+        System.out.println("==================");
+        System.out.println("BUS 1 Top Level");
+        System.out.println("BUS 2 Middle Level");
+        System.out.println("BUS 3 Regular Staff");
+        System.out.println("BUS 4 Staff");
+        System.out.println();
+        System.out.println("Masukan No NIK Karyawan");
+
+        Scanner karyawan = new Scanner(System.in);
+        i=karyawan.nextInt();
+
+        System.out.println("Anda Ditempatkan Di BUS KEBERAPA ?" + 1);
+        System.out.println("Jumlah BUS Top Level "+i);
+        System.out.println("Jumlah BUS Middle Level "+i);
+        System.out.println("Jumlah BUS Regular Staff "+i);
+        System.out.println("Jumlah BUS Staff "+i);
+
+
     }
 }
